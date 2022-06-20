a25eb7cceaa5b6149ec6920f5fd1be73556a3c76
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-06-20 16:31:21 -0700

    made a function to get the lat and long of a city

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,2 +1,27 @@
 const openWeatherAPI = "https://api.openweathermap.org/data/2.5/onecall?";
 const openWeatherMapAPI = "http://api.openweathermap.org/geo/1.0/direct?q=";
+const apiKey = "752a7c69c2f675bacc2d0b896d1432e5";
+let userInput = document.querySelector("#cityname");
+let submitBtn = document.getElementById("submitBtn");
+
+function getLL(city) {
+    let apiCall = openWeatherMapAPI + city + "&appid=" + apiKey;
+    console.log(apiCall);
+    fetch(apiCall)
+        .then(response => response.json())
+        .then(function(data) {
+            console.log(data);
+            localStorage.setItem("lat", data[0].lat);
+        })
+}
+
+function getWeather() {
+    if (!userInput.value) {
+        alert("Please, Enter a City name.");
+        return
+    }
+    console.log("test");
+    getLL(userInput.value);
+}
+
+submitBtn.addEventListener("click", getWeather);
