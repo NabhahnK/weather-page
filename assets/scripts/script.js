c161a5f5490787ff7da5c0bb471b0b097e3b0899
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-08-04 15:55:20 -0700

    wites current weather

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,11 +4,11 @@ const openWeatherMapAPI = "http://api.openweathermap.org/geo/1.0/direct?q=";
 const apiKey = "752a7c69c2f675bacc2d0b896d1432e5";
 let userInput = document.querySelector("#cityname");
 let submitBtn = document.getElementById("submitBtn");
-const cityAndDate = document.getElementById("city-date");
-const temp = document.getElementById("curr-temp");
-const humm = document.getElementById("curr-hum");
-const uv = document.getElementById("curr-uv");
-const wind = document.getElementById("curr-wind");
+const cityAndDate = document.querySelector("#city-date");
+const temp = document.querySelector("#curr-temp");
+const humm = document.querySelector("#curr-hum");
+const uv = document.querySelector("#curr-uv");
+const wind = document.querySelector("#curr-wind");
 const history = document.getElementById("history");
 
 // Creates the Btns for previous searched citys
@@ -29,7 +29,7 @@ function createBtns() {
 
                 document.getElementById("history").appendChild(btn);
             }
-            console.log(localStorage.getItem(localStorage.key(i)));
+            // console.log(localStorage.getItem(localStorage.key(i)));
         }
     } else {
         //Local storage is empty
@@ -37,11 +37,17 @@ function createBtns() {
     }
 }
 
-function fillHTML() {
+async function fillHTML(data) {
+    // return;
     // call in html variables to fill on the top
-
     // Fill in day w
-
+    // cityAndDate.innerHTM;
+    let dataV2 = data
+    temp.innerHTML = await "Temp: " + dataV2.current.temp;
+    await console.log(dataV2.current.wind_speed);
+    humm.textContent = await "Humidity: " + dataV2.current.humidity;
+    uv.textContent = await "UV: " + dataV2.current.uvi;
+    wind.textContent = "Wind: " + dataV2.current.wind_speed;
     // Loop to make and fill cards
 }
 
@@ -69,8 +75,9 @@ function getWeatherData() {
     fetch(apiCall)
         .then(response => response.json())
         .then(function (data) {
+            fillHTML(data);
             console.log(data);
-            localStorage.setItem("cityData", data);
+            // localStorage.setItem("cityData", data);
         })
 }
 
@@ -83,7 +90,6 @@ function ini(event) {
     }
     getLL(userInput.value);
     getWeatherData();
-    fillHTML();
 }
 
 createBtns()
